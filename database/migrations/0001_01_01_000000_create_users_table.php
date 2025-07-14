1afb26fdda70b01df163ce4f7dc7517080d3f395
Rifki Firansah <muhammadrifkifiransah@gmail.com>
2025-07-15 00:16:43 +0700

    feat: fix skema database with mysql

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['masyarakat', 'admin'])->default('masyarakat');
             $table->rememberToken();
             $table->timestamps();
         });
